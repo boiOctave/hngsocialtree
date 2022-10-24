aa01d7f023c4e911927578e2b263c57182a41906
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-24 14:12:21 +0100

    installed MUI and creted a json confg file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { StyledEngineProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline/CssBaseline';
 import './App.css';
+import Main from './views/Main';
 
 function App() {
-    return <div className='App'>hi</div>;
+    return (
+        <StyledEngineProvider injectFirst>
+            <Main />
+            <CssBaseline />;
+        </StyledEngineProvider>
+    );
 }
 
 export default App;
