b72cbe90d85ed98487277431878ff43f91534ecd
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-27 17:14:13 +0100

    changed from mui avatar tag to jsx img tag

--- src/components/Header/ProfileImage.js
+++ src/components/Header/ProfileImage.js
@@ -5,12 +5,13 @@ import profile__img from '../../assets/images/profile__img.png';
 const ProfileImage = ({ name }) => {
     return (
         <Box flex>
-            <Avatar
+            {/* <Avatar
                 alt='Profile Image'
                 id='profile__img'
                 src={profile__img}
                 sx={{ width: 56, height: 56 }}
-            />
+            /> */}
+            <img src={profile__img} alt='profile avatar' id='profile__img' />
             <Typography
                 sx={{
                     fontFamily: `'Roboto Condensed', sans-serif`,
