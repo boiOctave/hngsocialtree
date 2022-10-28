929ce7c0073a490830bd560582e47a9372faac95
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-28 20:49:34 +0100

    added remaining links

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -8,9 +8,17 @@ const index = () => {
             sx={{
                 marginTop: '3rem',
                 display: 'flex',
+                flexDirection: 'column',
+                gap: '1rem',
                 justifyContent: 'center',
+                alignItems: 'center',
             }}>
             <Links name={'Twitter Link'} />
+            <Links name={'Zuri Team'} />
+            <Links name={'Zuri Books'} />
+            <Links name={'Phython Books'} />
+            <Links name={'Background Checks for Coders'} />
+            <Links name={'Design Books'} />
         </Box>
     );
 };
