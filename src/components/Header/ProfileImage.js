63b7239bfd09595d3dc7053fb45da8238c3e95bf
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-28 15:51:08 +0100

    added css file

--- src/components/Header/ProfileImage.js
+++ src/components/Header/ProfileImage.js
@@ -1,17 +1,30 @@
 import { Avatar, Box, Typography } from '@mui/material';
 import React from 'react';
 import profile__img from '../../assets/images/profile__img.png';
+import './Header.css';
 
 const ProfileImage = ({ name }) => {
     return (
-        <Box flex>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                gap: '1rem',
+            }}>
             {/* <Avatar
                 alt='Profile Image'
                 id='profile__img'
                 src={profile__img}
                 sx={{ width: 56, height: 56 }}
             /> */}
-            <img src={profile__img} alt='profile avatar' id='profile__img' />
+            <img
+                src={profile__img}
+                alt='profile avatar'
+                id='profile__img'
+                className='profile__img'
+            />
             <Typography
                 sx={{
                     fontFamily: `'Roboto Condensed', sans-serif`,
