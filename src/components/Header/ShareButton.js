f5cff17c68568c833dea033f5de6cdbdbd5e28f3
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-28 16:06:14 +0100

    deleted avatar mui tag

--- src/components/Header/ShareButton.js
+++ src/components/Header/ShareButton.js
@@ -1,11 +1,13 @@
 import { IconButton } from '@mui/material';
 import React from 'react';
 import share from '../../assets/images/Type=Share, State=Default.svg';
-
+import './Header.css';
 const ShareButton = () => {
     return (
-        <IconButton aria-label='delete'>
-            <img src={share} alt='share' />{' '}
+        <IconButton
+            aria-label='delete'
+            sx={{ position: 'relative', bottom: '3rem' }}>
+            <img src={share} alt='share' className='share__button' />{' '}
         </IconButton>
     );
 };
