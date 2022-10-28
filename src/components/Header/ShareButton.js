d6c3e441479a16a7318fddc1d1b924cb908031af
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-28 15:03:37 +0100

    created share button

---
+++ src/components/Header/ShareButton.js
@@ -0,0 +1,9 @@
+import { IconButton } from '@mui/material';
+import React from 'react';
+import share from '../../assets/images/Type=Share, State=Default.svg';
+
+const ShareButton = () => {
+    return <IconButton aria-label='delete'>{share}</IconButton>;
+};
+
+export default ShareButton;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
+import { Box } from '@mui/material';
 import React from 'react';
 import ProfileImage from './ProfileImage';
+import ShareButton from './ShareButton';
 
 const Header = () => {
-    return <ProfileImage name={'Annette Black'} />;
+    return (
+        <Box>
+            <ProfileImage name={'Annette Black'} />
+            <ShareButton />
+        </Box>
+    );
 };
 
 export default Header;
