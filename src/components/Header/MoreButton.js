cd2bda613a96b2c6a9c8005431ced586ac9c87e4
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-10-29 11:23:36 +0100

    created state fore mobile share botton

--- src/components/Header/MoreButton.js
+++ src/components/Header/MoreButton.js
@@ -1,24 +1,56 @@
-import { IconButton, Tooltip } from '@mui/material';
-import React from 'react';
+import { IconButton } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useState } from 'react';
 import more from '../../assets/images/Type=more, State=Default.svg';
+import share from '../../assets/images/Type=Share, State=Default.svg';
 import './Header.css';
 const MoreButton = () => {
+    const [visibility, SetVisibility] = useState('hidden');
+    const onMore = () => {
+        if (visibility === 'hidden') {
+            SetVisibility('Visible');
+        } else {
+            SetVisibility('hidden');
+        }
+    };
     return (
-        <IconButton
-            className='more__button-container'
-            aria-label='delete'
-            sx={{
-                position: 'absolute',
-                bottom: '5rem',
-                width: '45px',
-                height: '45px',
-                right: '0rem',
-                '&:focus': {
-                    border: '3px solid #EBE9FE',
-                },
-            }}>
-            <img src={more} alt='share' className='more__button' />{' '}
-        </IconButton>
+        <div className='share__container'>
+            <IconButton
+                onClick={onMore}
+                className='more__button-container'
+                aria-label='delete'
+                sx={{
+                    position: 'absolute',
+                    bottom: '2rem',
+                    width: '45px',
+                    height: '45px',
+                    left: '5rem',
+
+                    '&:focus': {
+                        border: '3px solid #EBE9FE',
+                    },
+                }}>
+                <img src={more} alt='share' className='more__button' />{' '}
+            </IconButton>
+            <Box
+                className='share'
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    backgroundColor: '#EAECF0',
+                    padding: '0.2rem 1rem',
+                    borderRadius: '1rem',
+                    cursor: 'pointer',
+                    position: 'absolute',
+                    left: '-35px',
+                    bottom: '-35px',
+                    visibility: { visibility },
+                }}>
+                <img src={share} alt='share' className='mobile__share-button' />
+                <span className='span'>Share Page</span>
+            </Box>
+        </div>
     );
 };
 
