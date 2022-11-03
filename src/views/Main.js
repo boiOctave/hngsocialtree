eb6bbe16aa23cbcd5ad059764cb4b970012ea528
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-04 00:38:10 +0100

    refactor:footer

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,7 @@
-import { Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 import React from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+
 import Body from '../components/Body/index';
 
 const Main = () => {
@@ -9,7 +9,6 @@ const Main = () => {
         <Container maxWidth='xl' sx={{ marginTop: '5rem', height: '100vh' }}>
             <Header />
             <Body />
-            <Footer />
         </Container>
     );
 };
