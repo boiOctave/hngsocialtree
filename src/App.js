d285665489ae2929d14183900ddefe937f3001dc
boiOctave <105075630+boiOctave@users.noreply.github.com>
2022-11-03 15:11:45 +0100

    refactor: added react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { StyledEngineProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline/CssBaseline';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './views/Main';
 
@@ -7,6 +8,9 @@ function App() {
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
+            <Routes>
+                <Route path='/' element={<Main />} exact />
+            </Routes>
             <Main />
         </StyledEngineProvider>
     );
